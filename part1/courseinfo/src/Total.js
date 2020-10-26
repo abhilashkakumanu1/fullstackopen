0516f90d41c9c19b61205e51f0ad1672cb16babf
abhilashkakumanu1 <abhilashkakumanu1@gmail.com>
2020-10-26 19:44:53 +0530

    1.4

--- part1/courseinfo/src/Total.js
+++ part1/courseinfo/src/Total.js
@@ -1,5 +1,10 @@
-const Total = ({ exercises1, exercises2, exercises3 }) => {
-  return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>;
+const Total = ({ parts }) => {
+  const [part1, part2, part3] = parts;
+  return (
+    <p>
+      Number of exercises {part1.exercises + part2.exercises + part3.exercises}
+    </p>
+  );
 };
 
 export default Total;
