67d46b45ece0efa72678e3d2039e910402d83c6f
abhilashkakumanu1 <abhilashkakumanu1@gmail.com>
2020-10-26 19:33:59 +0530

    1.3

--- part1/courseinfo/src/Content.js
+++ part1/courseinfo/src/Content.js
@@ -1,13 +1,12 @@
 import React from "react";
 import Part from "./Part";
 
-const Content = (props) => {
-  const { part1, part2, part3, exercises1, exercises2, exercises3 } = props;
+const Content = ({ part1, part2, part3 }) => {
   return (
     <>
-      <Part part={part1} exercises={exercises1} />
-      <Part part={part2} exercises={exercises2} />
-      <Part part={part3} exercises={exercises3} />
+      <Part {...part1} />
+      <Part {...part2} />
+      <Part {...part3} />
     </>
   );
 };
