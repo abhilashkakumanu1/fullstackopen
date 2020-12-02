02cb424e67a216afe80192ebf4f472444840cb71
abhilashkakumanu1 <abhilashkakumanu1@gmail.com>
2020-12-02 10:20:25 +0530

    3.

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,7 +3,16 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
-app.use(morgan("tiny"));
+
+morgan.token("post-obj", function getObj(req) {
+  return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :post-obj"
+  )
+);
 
 let notes = [
   {
