a1b533dd2d19fc19cdbe90249729927b268c1a47
abhilashkakumanu1 <abhilashkakumanu1@gmail.com>
2020-12-12 05:58:15 +0530

    3.15

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -46,10 +46,10 @@ app.get("/api/persons/:id", (req, res) => {
 });
 
 app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  res.status(204).end();
+  const id = req.params.id;
+  Person.findByIdAndDelete(id)
+    .then((person) => res.status(204).end())
+    .catch((err) => console.log(err));
 });
 
 app.post("/api/persons", (req, res) => {
