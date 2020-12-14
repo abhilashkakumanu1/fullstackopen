e972e487321104c574424c8eba102f3ae64f59a2
abhilashkakumanu1 <abhilashkakumanu1@gmail.com>
2020-12-14 06:30:59 +0530

    updated error message

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -95,7 +95,7 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message);
 
   if (error.name === "CastError") {
-    return response.status(400).json({ error: "malformatted id" });
+    return response.status(400).json({ message: "malformatted id" });
   } else if (error.name === "ValidationError") {
     return response.status(400).json({ message: error.message });
   }
