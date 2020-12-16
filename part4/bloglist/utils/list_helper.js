fdb56d18f2ee04d972cfd8bd27a18708508f6efd
abhilashkakumanu1 <abhilashkakumanu1@gmail.com>
2020-12-16 09:44:07 +0530

    4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,5 +1,10 @@
 const dummy = (blogs) => 1;
 
+const totalLikes = (blogs) => {
+  return blogs ? blogs.reduce((total, curr) => total + curr.likes, 0) : 0;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
