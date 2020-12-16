c68e4d87920c4cd9bc9b7ecc4adc175607382a4e
abhilashkakumanu1 <abhilashkakumanu1@gmail.com>
2020-12-16 10:03:44 +0530

    4.5

--- part4/bloglist/tests/listHelper.test.js
+++ part4/bloglist/tests/listHelper.test.js
@@ -1,51 +1,59 @@
-const dummy = require("../utils/list_helper.js").dummy;
-const totalLikes = require("../utils/list_helper.js").totalLikes;
+const { dummy, totalLikes, favoriteBlog } = require("../utils/list_helper.js");
+
+const listWithOneBlog = [
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url:
+      "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0,
+  },
+];
+const listWithManyBlogs = [
+  {
+    _id: "5fd9549030a24e0a18b67eed",
+    author: "Abhilash Kakumanu",
+    title: "Guide to JavaScript Closures",
+    url: "https://stackabuse.com/guide-to-javascript-closures/",
+    likes: 6,
+    __v: 0,
+  },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url:
+      "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0,
+  },
+];
 
 test("dummy returns one", () => {
   expect(dummy()).toBe(1);
 });
 
 describe("total likes", () => {
-  const listWithOneBlog = [
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url:
-        "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-      likes: 5,
-      __v: 0,
-    },
-  ];
-  const listWithManyBlogs = [
-    {
-      _id: "5fd9549030a24e0a18b67eed",
-      author: "Abhilash Kakumanu",
-      title: "Guide to JavaScript Closures",
-      url: "https://stackabuse.com/guide-to-javascript-closures/",
-      likes: 6,
-      __v: 0,
-    },
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url:
-        "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-      likes: 5,
-      __v: 0,
-    },
-  ];
-
   test("of empty list is zero", () => {
     expect(totalLikes([])).toBe(0);
   });
 
-  test("of list with one blogs element is equal to the likes of that", () => {
+  test("of list with one blog element is equal to the likes of that", () => {
     expect(totalLikes(listWithOneBlog)).toBe(5);
   });
 
   test("of a bigger list is calculated right", () => {
     expect(totalLikes(listWithManyBlogs)).toBe(11);
+  });
+});
+
+describe("favorite blog", () => {
+  test("of list with one blog element is that element", () => {
+    expect(favoriteBlog(listWithOneBlog)).toEqual(listWithOneBlog[0]);
+  });
+  test("of a bigger list is the blog with most number of likes", () => {
+    expect(favoriteBlog(listWithManyBlogs)).toEqual(listWithManyBlogs[1]);
   });
 });
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -4,7 +4,12 @@ const totalLikes = (blogs) => {
   return blogs ? blogs.reduce((total, curr) => total + curr.likes, 0) : 0;
 };
 
+const favoriteBlog = (blogs) => {
+  return blogs.sort((a, b) => a.likes - b.likes)[blogs.length - 1];
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
